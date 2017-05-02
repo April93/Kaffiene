afdd5f64225f7f484744a9ee3343599791b83d1f
April93 <AprilMPapke@gmail.com>
2017-05-01 21:40:13 -0700

    Fixed badly formatted names from zeromeusers.py. Added/sorted by followers. Updated userdata.txt

--- zeromeusers.py
+++ zeromeusers.py
@@ -8,12 +8,23 @@ newsitesdb = '../1MeFqFfFFGQfa1J3gJyYYUvb5Lksczq7nH/merged-ZeroMe/ZeroMe.db'
 conn = sqlite3.connect(newsitesdb)
 c = conn.cursor()
 
+followdict = {}
+for row in c.execute('SELECT user_name, hub||"/"||auth_address, count(hub||"/"||auth_address) FROM follow GROUP by hub||"/"||auth_address ORDER BY count(hub||"/"||auth_address) desc'):
+	followdict[row[1]] = row[2]
+
 f = open("userdata.txt","w")
 for row in c.execute('SELECT cert_user_id, hub, directory, user_name, intro FROM json WHERE hub IS NOT null and site==hub'):
 	if row[0]:
 		f.write(row[0])
 	if row[3]:
-		f.write(" - "+row[3].encode("UTF-8"))
+		name = row[3].encode("UTF-8")
+		name = name.replace(':','')
+		name = name.replace('[','')
+		name = name.replace(']','')
+		name = name.replace("\n","")
+		if(len(name) >= 34):
+			name = name[:34]+"..."
+		f.write(" - "+name)
 	f.write("[")
 	if row[4]:
 		intro = row[4].encode("UTF-8")
@@ -34,7 +45,9 @@ for row in c.execute('SELECT cert_user_id, hub, directory, user_name, intro FROM
 	f.write(":")
 	if row[1] and row[2]:
 		#"Me.ZeroNetwork.bit/?Profile/"+
-		f.write(row[1]+"/"+row[2].replace('data/users/',''))
+		link = row[1]+"/"+row[2].replace('data/users/','')
+		f.write(link)
+		f.write(" "+str(followdict.get(link,'-')))
 	f.write("\n")
 
 f.close()
